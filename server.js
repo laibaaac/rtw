965ec5a506db0e716568d12de111c417350e6ff9
Laiba Choudhry <laiba@hotmail.nl>
2023-04-25 14:56:24 +0200

    werkt?

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 4545
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.resolve('public')));
 
 app.get('/', (req, res) => {
     res.render('index');
